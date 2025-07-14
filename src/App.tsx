390c0e16c97cf4904a30ebf9caa31054174ffbfd
ne1i <valentinfroment7@gmail.com>
2025-07-14 15:05:14 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { APITester } from "./APITester";
+import "./index.css";
+
+
+
+export function App() {
+  return (        
+      <h1 className="text-5xl font-bold my-4 leading-tight">MUSIC PLAYER</h1>
+  );
+}
+
+export default App;
